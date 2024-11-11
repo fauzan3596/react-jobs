c15bc7369cab1d48bcb3971158f7622d2ac57b18
fauzan3596 <fauzan3596@gmail.com>
2024-11-12 00:46:06 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import Navbar from "./components/Navbar";
+import ViewAllJobs from "./components/ViewAllJobs";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
+}
+
+export default App;
